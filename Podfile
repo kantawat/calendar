d99623835ae3a68a61121eb4939ea0a90fda7ac9
kantawat <ton.tey18@gmail.com>
2019-12-17 00:19:20 +0700

    calendar init

---
+++ Podfile
@@ -0,0 +1,23 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'calendar184' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for calendar184
+    # add pods for desired Firebase products
+    # https://firebase.google.com/docs/ios/setup#available-pods
+
+    pod 'Firebase/Database'
+    pod 'KDCalendar', '~> 1.8.2'
+  target 'calendar184Tests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'calendar184UITests' do
+    # Pods for testing
+  end
+
+end
--- calendar184/ViewController.swift
+++ calendar184/ViewController.swift
@@ -7,14 +7,90 @@
 //
 
 import UIKit
+import KDCalendar
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController , CalendarViewDataSource{
+    
+    @IBOutlet weak var calendarView: CalendarView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let style = CalendarView.Style()
+        
+        
+        style.cellShape                = .bevel(8.0)
+        style.cellColorDefault         = UIColor.clear
+        style.cellColorToday           = UIColor(red:1.00, green:0.84, blue:0.64, alpha:1.00)
+        style.cellSelectedBorderColor  = UIColor(red:1.00, green:0.63, blue:0.24, alpha:1.00)
+        style.cellEventColor           = UIColor(red:1.00, green:0.63, blue:0.24, alpha:1.00)
+        style.headerTextColor          = UIColor.gray
+        
+        style.cellTextColorDefault     = UIColor(red: 249/255, green: 180/255, blue: 139/255, alpha: 1.0)
+        style.cellTextColorToday       = UIColor.orange
+        style.cellTextColorWeekend     = UIColor(red: 237/255, green: 103/255, blue: 73/255, alpha: 1.0)
+        style.cellColorOutOfRange      = UIColor(red: 249/255, green: 226/255, blue: 212/255, alpha: 1.0)
+        
+        style.headerBackgroundColor    = UIColor.white
+        style.weekdaysBackgroundColor  = UIColor.white
+        style.firstWeekday             = .sunday
+        
+        style.locale                   = Locale(identifier: "en_US")
+        
+        style.timeZone                 = TimeZone(abbreviation: "UTC")!
+        
+        style.cellFont = UIFont(name: "Helvetica", size: 20.0) ?? UIFont.systemFont(ofSize: 20.0)
+        style.headerFont = UIFont(name: "Helvetica", size: 20.0) ?? UIFont.systemFont(ofSize: 20.0)
+        style.weekdaysFont = UIFont(name: "Helvetica", size: 14.0) ?? UIFont.systemFont(ofSize: 14.0)
+        
+        calendarView.style = style
+        
+        calendarView.dataSource = self
+        
+        calendarView.direction = .horizontal
+        calendarView.multipleSelectionEnable = false
+        calendarView.marksWeekends = true
+        
+        
+        calendarView.backgroundColor = UIColor(red: 252/255, green: 252/255, blue: 252/255, alpha: 1.0)
+        
     }
-
-
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let today = Date()
+        self.calendarView.setDisplayDate(today, animated: false)
+    }
+    
+    func startDate() -> Date {
+        
+        var dateComponents = DateComponents()
+        dateComponents.month = -12
+        
+        let today = Date()
+        
+        let threeMonthsAgo = self.calendarView.calendar.date(byAdding: dateComponents, to: today)!
+        
+        return threeMonthsAgo
+    }
+    
+    func endDate() -> Date {
+        
+        var dateComponents = DateComponents()
+        
+        dateComponents.month = 12
+        let today = Date()
+        
+        let twoYearsFromNow = self.calendarView.calendar.date(byAdding: dateComponents, to: today)!
+        
+        return twoYearsFromNow
+        
+    }
+    
+    
+    func headerString(_ date: Date) -> String? {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        let myString = formatter.string(from: date as Date)
+        return myString
+    }
+    
+    
 }
-
